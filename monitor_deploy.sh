6899055b76d99c94b98462d9afd9afd0481d14a9
root <root@localhost.(none)>
2016-10-20 08:44:38 -0400

    monitor the ipvt directory and deploy them when its update

--- monitor_deploy.sh
+++ monitor_deploy.sh
@@ -4,12 +4,28 @@
 #last_size=`ls -l $file |awk '{print $5}'`
 
 projectDir=/home
+destDir=/opt/mcu/
+
+deploy(){
+	ansible-playbook /opt/ipvt-deploy/copy.yml -e "package=$file" 
+}
+
 
 inotifywait -mrq -e close_write --format %f "$projectDir"|while read file
 do
 cd $projectDir
 #wput $file ftp://test:pass@192.168.121.201/log/ssli/
 #echo $file
+
+deploy 
+
+#if [[ ! -d $destDir ]];then
+#	cd $destDir && tar zxf $file
+#else
+#	rm -r $destDir/release && tar zxf $file
+#	
+#fi
+	
 
 done
 
