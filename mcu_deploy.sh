ee71944eb707a871f023dc1ebd26dd968882b8df
root <root@localhost.(none)>
2016-10-31 11:31:59 +0800

    new version for mcu deploy script

--- mcu_deploy.sh
+++ mcu_deploy.sh
@@ -2,17 +2,27 @@
 mcuDir=/opt/mcu/release
 deployDir=/opt/mcu
 
-dateExt=`date +%F`
+dateExt=`date +%Y%m%d%s`
 file=$1
 
-if [[ -d $mcuDir ]];then
+pidFile=`pidof gs_avs`
+
+untar(){
+	mv $mcuDir $mcuDir-$dateExt
+	tar zxf $deployDir/$file.tar.gz -C $deployDir
+	cd $mcuDir && setsid ./gs_avs &
+}
+
+if [[ $pidFile != '' ]];then
 	
 	kill -9 `pidof gs_avs`
-	mv $mcuDir $mcuDir-$dateExt
-else	
-	tar zxf $deployDir/$file.tar.gz -C $deployDir
-	cd $mcuDir && ./gs_avs &
+	untar;
 	sleep 10
-	echo `$mcuDir/mcudump ver|awk '{print $3}'`
+	echo "MCU Version:"`$mcuDir/mcudump ver|awk '{print $3}'`
+else	
+	echo 'MCU process NO exist!'
+	untar;
+	sleep 10
+	echo "MCU Version:"`$mcuDir/mcudump ver|awk '{print $3}'`
 	
 fi
